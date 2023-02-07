b2e8d6d8fa4c8297555ccd2e09ad952e732ca5e1
blessy <blessysusancheriyan28@gmail.com>
2023-02-07 15:28:24 +0530

    first commit

---
+++ myapps/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Products from './components/Products/Products';
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+    
+<Products></Products>
+ 
+    </div>
+  );
+}
+
+export default App;
